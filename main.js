0530e2c7f72bfd61e73042a2bbd65eacdd6b33a8
mariacoutinhoo <131712076+mariacoutinhoo@users.noreply.github.com>
2023-11-13 16:32:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-function tocarTeclaAplausos(){
-    document.querySelector('.som_tecla_aplausos').onclick = tocarTeclaAplausos;
+function tocaSomRisada(){
+    document.querySelector("#som_tecla_risada").play();   
 }
-function tocarTeclaVaia(){
-    document.querySelector('.som_tecla_vaia').onclick = tocarTeclaVaia;
-}
-function tocarTeclaRisada(){
-    document.querySelector('.som_tecla_risada').onclick = tocarTeclaRisada;
-}
-function tocarTeclaErrado(){
-    document.querySelector('.som_tecla_errado').onclick = tocarTeclaErrado;
-}
-function tocarTeclaGrilo(){
-    document.querySelector('.som_tecla_grilo').onclick = tocarTeclaGrilo;
-}
-function tocarTeclaTambores(){
-    document.querySelector('.som_tecla_tambores').onclick = tocarTeclaTambores;
-}
-function tocarTeclaTrombeta(){
-    document.querySelector('.som_tecla_trombeta').onclick = tocarTeclaTrombeta;
-}
-function tocarTeclaDinheiro(){
-    document.querySelector('.som_tecla_dinheiro').onclick = tocarTeclaDinheiro;
-}
-function tocarTeclaVitoria(){
-    document.querySelector('.som_tecla_vitoria').onclick = tocarTeclaVitoria;
-}
+
+document.querySelectorAll('.tecla');
+
+
